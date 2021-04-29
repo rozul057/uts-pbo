1d5a5ef657c9be5017cf7b2ccdf1a7ef88635644
rozul057 <82423136+rozul057@users.noreply.github.com>
2021-04-28 22:16:49 -1200

    Create class1.java

---
+++ class1.java
@@ -0,0 +1,22 @@
+package utspbo;
+public class Vehicle {
+    private String maker;
+    private int kmperGallon;
+    
+    void Vehicle(String maker, int kmperGallon){
+        
+    }
+    public void setmaker(String maker){
+        this.maker = maker;
+    }
+    public String getmaker(){
+        return this.maker;
+    }  
+    
+    public void setkmperGallon(int kmperGallon){
+        this.kmperGallon = kmperGallon;
+    }
+    public int getkmperGallon(){
+        return this.kmperGallon;
+    }  
+}
